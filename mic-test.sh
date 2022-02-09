7f92ac621ba835c30cdcaf1f88b7d924bfee1501
vavrek <vavrek@vavrek.com>
2022-02-09 12:27:08 +0100

    Formatting microphone test script

--- mic-test.sh
+++ mic-test.sh
@@ -14,7 +14,7 @@ printf "\nLaunching recording test...\n\nRecognized input devices: \n\n"
 
 arecord -l
 
-printf "\nRecording for 5 seconds.\nPlease speak into your microphone...\n\n"
+printf "\nRecording for 5 seconds.\n\nPlease speak into your microphone...\n\n"
 
 arecord -f S16_LE -c 2 -r 192000 -d 5 mic-test.wav
 
@@ -22,5 +22,5 @@ printf "\nComplete. Playing recording...\n\n"
 
 aplay -D hw:0,0 mic-test.wav
 
-printf "\nIf you do not hear anything, please check your microphone input and speaker output levels.\n\n"
+printf "\nIf you do not hear anything, please check your microphone input levels and speaker volume.\n\n"
 
